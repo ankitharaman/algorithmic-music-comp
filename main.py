cfb56da54b178dc46f41f9552abfcededd2160e4
Amitav Nott <58123848+anott03@users.noreply.github.com>
2025-04-14 11:56:19 -0400

    save preprocessed data to pkl files

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
+#!/usr/bin/env python
+
 import preprocessing
 from music21 import *
+import pickle
 
 
 def main():
-    # file = "./scores/odetojoy.mxl"
-    file = "./scores/Spring.mxl"
-    s = converter.parse(file)
-    m, h = processed_data = preprocessing.prepare_data(s)
-    print(h)
+    positiveSongs = ["./scores/canon.mxl",
+                     "./scores/odetojoy.mxl",
+                     "./scores/VAIANA_piano_solo_de_Disney.mxl",
+                     "./scores/Spring.mxl"]
+
+    negativeSongs = ["./scores/Sonate_No._14_Moonlight_1st_Movement.mxl",
+                     "./scores/Five_Hundred_Miles_2.mxl",
+                     "./scores/swan_lake.mxl",
+                     "./scores/Chopin_Prelude_in_E_Minor_Opus_28_No_4.mxl"]
+
+    processedPositiveSongs = []
+    processedNegativeSongs = []
+
+    for song in positiveSongs:
+        s = converter.parse(song)
+        mh_pair = preprocessing.prepare_data(s)
+        processedPositiveSongs.append(mh_pair)
+
+    for song in negativeSongs:
+        s = converter.parse(song)
+        mh_pair = preprocessing.prepare_data(s)
+        processedNegativeSongs.append(mh_pair)
+    
+    positive_file = open("positive.pkl", "wb")
+    negative_file = open("negative.pkl", "wb")
+    pickle.dump(processedPositiveSongs, positive_file)
+    pickle.dump(processedNegativeSongs, negative_file)
+    positive_file.close()
+    negative_file.close()
 
 
 if __name__ == "__main__":
